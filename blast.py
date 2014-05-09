fa02cb63a37e1cd6a5465893609599860ce2562d
Vijay Ramakrishnan <vijay120@gmail.com>
2014-05-09 01:47:39 -0700

    sent src

--- blast.py
+++ blast.py
@@ -5,17 +5,16 @@ from select import select
 from threading import Thread, Timer
 import time
 
-UDP_PORT = 4019
+UDP_PORT_SENDER = 4023
+UDP_PORT_RECEIVER = 4024
 UDP_IP = "127.0.0.1"
 
 PROTO = "prot"
 NODATA = ""
 DATA = 2
+SRC = 1
 SIZEOFBYTE = 8
 NUMFRAGS = 32
-
-def hello():
-    print "hello, world"
 
 def fragment_factory(MessageId, DataLength, NumFrags, PacketType, FragMask, Data):
 	binProto = ''.join(format(ord(x), 'b') for x in PROTO) + "0000"
@@ -23,7 +22,7 @@ def fragment_factory(MessageId, DataLength, NumFrags, PacketType, FragMask, Data
 	binDataLength = '{0:032b}'.format(DataLength)
 	binNumFrags = '{0:016b}'.format(NumFrags)
 	binPacketType = '{0:016b}'.format(PacketType)
-	binFragMask = '{0:032b}'.format(1 << FragMask)
+	binFragMask = FragMask
 	binData = ''.join('{:08b}'.format(ord(c)) for c in Data)
 	return binProto + binMessageId + binDataLength + binNumFrags + binPacketType + binFragMask + binData
 
@@ -51,13 +50,18 @@ def decode_fragment(fragment):
 def sender():
 
 	sockSend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
- 	
+ 	sockSend.bind((UDP_IP, UDP_PORT_SENDER))
+
  	# step 1
  	for i in range(0, NUMFRAGS):
  		MessageId = 1
  		Length = 100
- 		fragment = fragment_factory(MessageId, Length, NUMFRAGS, 2, i, "Hello: " + str(i))
- 		sockSend.sendto(fragment, (UDP_IP, UDP_PORT))
+ 		fragment = fragment_factory(MessageId, Length, NUMFRAGS, DATA, '{0:032b}'.format(1 << i), "Hello: " + str(i))
+ 		sockSend.sendto(fragment, (UDP_IP, UDP_PORT_RECEIVER))
+
+ 	data, addr = sockSend.recvfrom(65535)
+ 	print "at sender"
+ 	print data
 
 	sockSend.close()
 	print "sender closed"
@@ -65,7 +69,7 @@ def sender():
 
 def receiver():
 	sockRec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sockRec.bind((UDP_IP, UDP_PORT))
+	sockRec.bind((UDP_IP, UDP_PORT_RECEIVER))
 	received = False
 	counter = 0
 	ListOfMessages = []
@@ -83,7 +87,7 @@ def receiver():
 
 	bitFragsArrived = []
 	for i in range(0, NumFrags):
-		bitFragsArrived.append(False)
+		bitFragsArrived.append('0')
 
 	for data in ListOfMessages:
 
@@ -98,20 +102,25 @@ def receiver():
 		if Dict["PacketType"] != DATA:
 			print "invalid message"
 
-		bitFragsArrived[Dict["FragMask"]] = True
+		bitFragsArrived[Dict["FragMask"]] = '1'
 
 		print str(Dict["MessageId"]) + ", " + str(Dict["FragMask"] + 1) + " of " + str(Dict["NumFrags"]) + " : " + Dict["Data"]
 
+
+	FragMaskToSend = "".join(bitFragsArrived)
+	print FragMaskToSend
+
+	SrcMessage = fragment_factory(1, 100, 1, SRC, FragMaskToSend, "")
+	print SrcMessage
+
+	sockRec.sendto(SrcMessage, (UDP_IP, UDP_PORT_SENDER))
 
 	sockRec.close()
 	print "receiver closed"
 
 	
 if __name__ == "__main__":
-
-	# t = Timer(1.0, hello)
-	# t.start()
 	receiver = Thread(target=receiver)
-	receiver.start()
 	sender = Thread(target=sender)
+	receiver.start()
 	sender.start()
