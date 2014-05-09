72e738db0017bcd39a3cbf85c2fac3c99ec0b48a
jlim <jlim@rubiconproject.com>
2014-05-09 01:17:52 -0700

    added timer

--- blast.py
+++ blast.py
@@ -2,7 +2,7 @@ import socket
 import base64
 import sys
 from select import select
-from threading import Thread
+from threading import Thread, Event, Timer
 import time
 
 UDP_PORT = 4019
@@ -96,10 +96,86 @@ def receiver():
 	sockRec.close()
 	print "receiver closed"
 
+
+
+def TimerReset(*args, **kwargs):
+    """ Global function for Timer """
+    return _TimerReset(*args, **kwargs)
+
+
+class _TimerReset(Thread):
+    """Call a function after a specified number of seconds:
+
+    t = TimerReset(30.0, f, args=[], kwargs={})
+    t.start()
+    t.cancel() # stop the timer's action if it's still waiting
+    """
+
+    def __init__(self, interval, function, args=[], kwargs={}):
+        Thread.__init__(self)
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.finished = Event()
+        self.resetted = True
+
+    def cancel(self):
+        """Stop the timer if it hasn't finished yet"""
+        self.finished.set()
+
+    def run(self):
+        print "Time: %s - timer running..." % time.asctime()
+
+        while self.resetted:
+            print "Time: %s - timer waiting for timeout in %.2f..." % (time.asctime(), self.interval)
+            self.resetted = False
+            self.finished.wait(self.interval)
+
+        if not self.finished.isSet():
+            self.function(*self.args, **self.kwargs)
+        self.finished.set()
+        print "Time: %s - timer finished!" % time.asctime()
+
+    def reset(self, interval=None):
+        """ Reset the timer """
+
+        if interval:
+            print "Time: %s - timer resetting to %.2f..." % (time.asctime(), interval)
+            self.interval = interval
+        else:
+            print "Time: %s - timer resetting..." % time.asctime()
+
+        self.resetted = True
+        self.finished.set()
+        self.finished.clear()
+
+
+
+#
+# Usage examples
+#
+def hello():
+    print "Time: %s - hello, world" % time.asctime()
 	
 if __name__ == "__main__":
 
-	receiver = Thread(target=receiver)
-	receiver.start()
-	sender = Thread(target=sender)
-	sender.start()
+	#receiver = Thread(target=receiver)
+	#receiver.start()
+	#sender = Thread(target=sender)
+	#sender.start()
+
+	print "Time: %s - start..." % time.asctime()
+	tim = TimerReset(5, hello)
+	tim.start()
+	#print "Time: %s - sleeping for 4..." % time.asctime()
+	time.sleep (4)
+	tim.reset()
+	time.sleep(10)
+	tim = TimerReset(4,hello)
+	tim.start()
+	#print "Time: %s - sleeping for 10..." % time.asctime()
+	#time.sleep (10)
+	#print "Time: %s - end..." % time.asctime()
+
+	print "\n\n"
